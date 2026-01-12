846411c9d8e0a5972ab6bf497f7cb6ccf61957f1
khmilevoi <Khmilevoi@gmail.com>
2026-01-12 16:03:10 +0100

    Align widget registry with upstream

---
+++ src/Widgets/CustomWidgets.ts
@@ -0,0 +1,34 @@
+import { FormContextType, RegistryWidgetsType, RJSFSchema, StrictRJSFSchema } from '@rjsf/utils';
+import AltDateWidget from './AltDateWidget';
+import AltDateTimeWidget from './AltDateTimeWidget';
+import ColorWidget from './ColorWidget';
+import DateTimeWidget from './DateTimeWidget';
+import DateWidget from './DateWidget';
+import EmailWidget from './EmailWidget';
+import PasswordWidget from './PasswordWidget';
+import TextWidget from './TextWidget';
+import UpDownWidget from './UpDownWidget';
+import URLWidget from './URLWidget';
+
+export function generateCustomWidgets<
+  T = any,
+  S extends StrictRJSFSchema = RJSFSchema,
+  F extends FormContextType = any
+>(): RegistryWidgetsType<T, S, F> {
+  return {
+    AltDateWidget,
+    AltDateTimeWidget,
+    ColorWidget,
+    DateWidget,
+    DateTimeWidget,
+    EmailWidget,
+    PasswordWidget,
+    TextWidget,
+    UpDownWidget,
+    URLWidget,
+  } as RegistryWidgetsType<T, S, F>;
+}
+
+export const CustomWidgets = generateCustomWidgets();
+
+export default generateCustomWidgets();
