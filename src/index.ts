0a67f40a960b4a9aeebb1fa663f04b0608d31064
khmilevoi <Khmilevoi@gmail.com>
2026-01-13 13:28:39 +0100

    Refactor widgets module structure

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export { default as Form, generateForm } from './MuiForm';
 export { default as Templates, generateTemplates } from './Templates';
 export { default as Theme, generateTheme } from './Theme';
 export { default as Widgets, generateWidgets } from './Widgets';
+export { default as CustomWidgets, generateCustomWidgets } from './CustomWidgets';
 
 export default MuiForm;
